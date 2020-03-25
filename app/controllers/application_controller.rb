8b1f7c86fcb0e94f5ac110973888caca69cc4664
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-25 01:37:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,51 +9,43 @@ class ApplicationController < Sinatra::Base
   end
 
    get '/' do
-
-  end
-
-  get '/' do
-
-  end
-
-  get '/articles' do
+   end 
+   
+  get "/articles" do
     @articles = Article.all
-    erb :'articles/index'
+    erb :index
   end
 
-  get '/articles/new' do
-    erb :'articles/new'
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
   end
 
-  get '/articles/:id' do
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  get "/articles/:id" do
     @article = Article.find(params[:id])
-    erb :'articles/show'
+    erb :show
   end
 
-  post '/articles' do
-    @article = Article.create(title: params[:title], content: params[:content])
-    redirect "/articles/#{@article.id}"
-  end
-
-  get '/articles/:id/edit' do
+  get "/articles/:id/edit" do
     @article = Article.find(params[:id])
-    erb :'articles/edit'
+    erb :edit
   end
 
-  patch '/articles/:id' do
+  patch "/articles/:id" do
     @article = Article.find(params[:id])
-    new_article = {}
-    new_article[:title] = params[:title]
-    new_article[:content] = params[:content]
-    @article.update(new_article)
-
-    redirect "/articles/#{params[:id]}"
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
   end
 
-  delete '/articles/:id' do
-    @article = Article.find(params[:id])
-    @article.destroy
-
-    redirect '/articles'
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
   end
-end
+
+
+end 
